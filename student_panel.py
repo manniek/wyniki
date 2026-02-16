a6ca47bdc5bc0038100af37f784438a68a93bc2d
manniek <103125460+manniek@users.noreply.github.com>
2026-02-16 22:46:03 +0100

    Update student_panel.py

--- student_panel.py
+++ student_panel.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import re
 import pandas as pd
-
+# --- TYMCZASOWY PODGLĄD INDEKSÓW (DO USUNIĘCIA POTEM) ---
+    st.write("---")
+    st.write("🔍 **Podgląd techniczny kolumn (sprawdź indeks 16):**")
+    for i, val in enumerate(dane):
+        st.write(f"Indeks {i}: `{val}`")
+    # -------------------------------------------------------
 def show_panel(wiersz_ucznia):
     # 1. GÓRNY PASEK (Przywrócony układ z przyciskiem po prawej)
     c_pow, c_spacer, c_btn = st.columns([6, 2, 2])
@@ -73,10 +78,3 @@ def show_panel(wiersz_ucznia):
         # Jeśli suma przekracza próg, pokazujemy złoty puchar
         if suma_total >= 40.5:
             st.success(f"🏆 **Zdobyte punkty:** {suma_total} (ZALICZONE!)")
-    # --- TYMCZASOWY PODGLĄD INDEKSÓW (DO USUNIĘCIA POTEM) ---
-    st.write("---")
-    st.write("🔍 **Podgląd techniczny kolumn (sprawdź indeks 16):**")
-    for i, val in enumerate(dane):
-        st.write(f"Indeks {i}: `{val}`")
-    # -------------------------------------------------------
-
