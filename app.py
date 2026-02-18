756640713c26108c91e9eda9440a1eec02ebb157
manniek <103125460+manniek@users.noreply.github.com>
2026-02-18 21:05:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -47,9 +47,24 @@ if not st.session_state.zalogowany:
                         st.session_state.update({"zalogowany": True, "rola": "uczen", "dane": df_w.iloc[[idx]]})
                         st.rerun()
             st.error("Błędny login lub hasło.")
+# --- WYŚWIETLANIE ---
 else:
     df_w, _ = wczytaj_dane()
     if st.session_state.rola == "admin":
         admin_panel.show_panel(df_w)
     else:
-        student_panel.show_panel(st.session_state.dane)
+        # ROZDZIELACZ MOBILNY
+        from streamlit_javascript import st_javascript
+        width = st_javascript("window.innerWidth")
+
+        if width is not None:
+            if width < 700:
+                # Jeśli ekran jest mały, ładujemy panel mobilny
+                import mobile_panel
+                mobile_panel.show_mobile_panel(st.session_state.dane)
+            else:
+                # Jeśli ekran jest duży, standardowy panel
+                student_panel.show_panel(st.session_state.dane)
+        else:
+            # Placeholder, dopóki JS nie zmierzy ekranu
+            st.info("Inicjalizacja widoku...")
